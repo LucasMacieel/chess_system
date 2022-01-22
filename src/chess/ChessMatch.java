e22ab03ff43c698836e4629d51d584cb13339742
Lucas Maciel <lucasmacielsousa29901@gmail.com>
2022-01-22 19:27:51 -0300

    Promotion Improvement

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -5,7 +5,6 @@ import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.*;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -117,14 +116,14 @@ public class ChessMatch {
             throw new IllegalStateException("There is no piece to be promoted");
         }
         if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
         Piece p = board.removePiece(pos);
         piecesOnTheBoard.remove(p);
 
-        ChessPiece newPiece = newPiece(type, promoted.getColor())
+        ChessPiece newPiece = newPiece(type, promoted.getColor());
         board.placePiece(newPiece, pos);
         piecesOnTheBoard.add(newPiece);
 
